345465b16976f8fb91d7b17e94706ff02c691a72
nobutnk <nobutnk@gmail.com>
2016-05-25 02:05:40 +0900

    JSR310設定の修正

--- src/test/java/tutorial/app/test/DateTimeApiTest.java
+++ src/test/java/tutorial/app/test/DateTimeApiTest.java
@@ -2,14 +2,23 @@ package tutorial.app.test;
 
 import java.io.IOException;
 
+import javax.inject.Inject;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:test-context.xml" })
 public class DateTimeApiTest {
+    
+    @Inject
+    ObjectMapper objectMapper;
 
     @Test
     public void test() throws JsonParseException, JsonMappingException, IOException {
@@ -17,9 +26,7 @@ public class DateTimeApiTest {
         String jsonString =
                 "{\"user_name\":\"Tanaka\", \"created_at\":\"2016-05-24 11:11:11.999\"}";
         
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JSR310Module());
-        SampleBean bean  = mapper.readValue(jsonString, SampleBean.class);
+        SampleBean bean  = objectMapper.readValue(jsonString, SampleBean.class);
         
         System.out.println(bean.getName());
         System.out.println(bean.getCreatedAt());
